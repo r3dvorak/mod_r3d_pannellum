4eb71938dc894d08f5692a2b35d308d73a191abc
r3dvorak <github@r3d.de>
2025-09-04 08:58:49 +0200

    c5.0.1 and enhance helper functionality for absolute panorama paths

--- helper.php
+++ helper.php
@@ -2,15 +2,17 @@
 /**
  * @package     Joomla.Module
  * @subpackage  mod_r3d_pannellum
- * @creation    2025-09-03
+ * @creation    2025-09-04
  * @author      Richard Dvorak, r3d.de
  * @copyright   Copyright (C) 2025 Richard Dvorak, https://r3d.de
  * @license     GNU GPL v3 or later (https://www.gnu.org/licenses/gpl-3.0.html)
- * @version     5.0.0
+ * @version     5.0.1
  * @file        modules/mod_r3d_pannellum/helper.php
  */
 
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Uri\Uri;
 
 /**
  * Helper class for R3D Pannellum Module
@@ -18,20 +20,35 @@ defined('_JEXEC') or die;
 class ModR3dPannellumHelper
 {
     /**
-     * Build config JSON for Pannellum
+     * Build config JSON for Pannellum.
+     *
+     * Ensures the panorama path becomes an absolute URL so it won't be affected
+     * by page base paths or subdirectory installs.
      *
      * @param   \Joomla\Registry\Registry  $params  Module parameters
      * @return  string  JSON configuration for viewer
      */
     public static function getConfig($params)
     {
-        // Basic setup only (mock Intermediate/Advanced)
+        // Get panorama parameter; default to the demo image installed under /media/
+        $panorama = (string) $params->get('panorama', 'media/mod_r3d_pannellum/demo.jpg');
+
+        // If it's not already absolute, prefix with the site root
+        if (
+            $panorama !== '' &&
+            strpos($panorama, 'http://') !== 0 &&
+            strpos($panorama, 'https://') !== 0 &&
+            strpos($panorama, '//') !== 0
+        ) {
+            $panorama = rtrim(Uri::root(), '/') . '/' . ltrim($panorama, '/');
+        }
+
         $config = [
-            'type'     => 'equirectangular',
-            'panorama' => $params->get('panorama', 'media/mod_r3d_pannellum/demo.jpg'),
+            'type' => 'equirectangular',
+            'panorama' => $panorama,
             'autoLoad' => (bool) $params->get('autoload', 1),
         ];
 
-        return json_encode($config);
+        return json_encode($config, JSON_UNESCAPED_SLASHES);
     }
 }
