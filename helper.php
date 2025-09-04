fa07938dcfc5bdedd2402758bbd7105c42d97cda
r3dvorak <github@r3d.de>
2025-09-04 09:17:47 +0200

    v5.0.3 and enhance configuration options for R3D Pannellum module

--- helper.php
+++ helper.php
@@ -6,7 +6,7 @@
  * @author      Richard Dvorak, r3d.de
  * @copyright   Copyright (C) 2025 Richard Dvorak, https://r3d.de
  * @license     GNU GPL v3 or later (https://www.gnu.org/licenses/gpl-3.0.html)
- * @version     5.0.1
+ * @version     5.0.2
  * @file        modules/mod_r3d_pannellum/helper.php
  */
 
@@ -20,20 +20,15 @@ use Joomla\CMS\Uri\Uri;
 class ModR3dPannellumHelper
 {
     /**
-     * Build config JSON for Pannellum.
-     *
-     * Ensures the panorama path becomes an absolute URL so it won't be affected
-     * by page base paths or subdirectory installs.
+     * Build config JSON for Pannellum (Basic + extras).
      *
      * @param   \Joomla\Registry\Registry  $params  Module parameters
      * @return  string  JSON configuration for viewer
      */
     public static function getConfig($params)
     {
-        // Get panorama parameter; default to the demo image installed under /media/
+        // Resolve panorama to absolute URL (avoids base path issues)
         $panorama = (string) $params->get('panorama', 'media/mod_r3d_pannellum/demo.jpg');
-
-        // If it's not already absolute, prefix with the site root
         if (
             $panorama !== '' &&
             strpos($panorama, 'http://') !== 0 &&
@@ -43,12 +38,83 @@ class ModR3dPannellumHelper
             $panorama = rtrim(Uri::root(), '/') . '/' . ltrim($panorama, '/');
         }
 
-        $config = [
+        $cfg = [
             'type' => 'equirectangular',
             'panorama' => $panorama,
             'autoLoad' => (bool) $params->get('autoload', 1),
         ];
 
-        return json_encode($config, JSON_UNESCAPED_SLASHES);
+        // --- Initial view ---
+        $yaw = $params->get('yaw', '');
+        $pitch = $params->get('pitch', '');
+        $hfov = $params->get('hfov', '');
+        if ($yaw !== '') {
+            $cfg['yaw'] = (float) $yaw;
+        }
+        if ($pitch !== '') {
+            $cfg['pitch'] = (float) $pitch;
+        }
+        if ($hfov !== '') {
+            $cfg['hfov'] = (float) $hfov;
+        }
+
+        $minH = $params->get('min_hfov', '');
+        $maxH = $params->get('max_hfov', '');
+        if ($minH !== '') {
+            $cfg['minHfov'] = (float) $minH;
+        }
+        if ($maxH !== '') {
+            $cfg['maxHfov'] = (float) $maxH;
+        }
+
+        // --- Auto-rotate ---
+        $ar = $params->get('auto_rotate', '');
+        if ($ar !== '' && (float) $ar !== 0.0) {
+            $cfg['autoRotate'] = (float) $ar; // deg/sec; +ccw, -cw
+            $arIdle = (int) $params->get('auto_rotate_inactivity', 0);
+            $arStop = (int) $params->get('auto_rotate_stop', 0);
+            if ($arIdle > 0) {
+                $cfg['autoRotateInactivityDelay'] = $arIdle;
+            }
+            if ($arStop > 0) {
+                $cfg['autoRotateStopDelay'] = $arStop;
+            }
+        }
+
+        // --- Controls & Interaction ---
+        $cfg['showZoomCtrl'] = (bool) $params->get('show_zoom_ctrl', 1);
+        $cfg['showFullscreenCtrl'] = (bool) $params->get('show_fullscreen_ctrl', 1);
+
+        $dcZoom = $params->get('double_click_zoom', '');
+        if ($dcZoom !== '') {
+            $cfg['doubleClickZoom'] = (bool) $dcZoom;
+        }
+
+        $mouseZoom = $params->get('mouse_zoom', '');
+        if ($mouseZoom !== '') {
+            $cfg['mouseZoom'] = (bool) $mouseZoom;
+        }
+
+        $disableKb = $params->get('disable_keyboard_ctrl', '');
+        if ($disableKb !== '') {
+            $cfg['disableKeyboardCtrl'] = (bool) $disableKb;
+        }
+
+        $draggable = $params->get('draggable', '');
+        if ($draggable !== '') {
+            $cfg['draggable'] = (bool) $draggable;
+        }
+
+        // --- Compass ---
+        $compass = $params->get('compass', '');
+        if ($compass !== '') {
+            $cfg['compass'] = (bool) $compass;
+        }
+        $north = $params->get('north_offset', '');
+        if ($north !== '') {
+            $cfg['northOffset'] = (float) $north;
+        }
+
+        return json_encode($cfg, JSON_UNESCAPED_SLASHES);
     }
 }
--- tmpl/default.php
+++ tmpl/default.php
@@ -2,27 +2,33 @@
 /**
  * @package     Joomla.Module
  * @subpackage  mod_r3d_pannellum
- * @creation    2025-09-03
+ * @creation    2025-09-04
  * @author      Richard Dvorak, r3d.de
  * @copyright   Copyright (C) 2025 Richard Dvorak, https://r3d.de
  * @license     GNU GPL v3 or later (https://www.gnu.org/licenses/gpl-3.0.html)
- * @version     5.0.0
+ * @version     5.0.3
  * @file        modules/mod_r3d_pannellum/tmpl/default.php
  */
 
 defined('_JEXEC') or die;
 
 $containerId = 'pannellum_' . $module->id;
-?>
 
-<div id="<?php echo $containerId; ?>" style="width:100%;height:500px;"></div>
+// CSS size from params (allow any valid CSS unit)
+$width = $params->get('container_width', '100%');
+$height = $params->get('container_height', '500px');
+
+// basic sanitization for style attribute
+$style = 'width:' . htmlspecialchars($width, ENT_QUOTES, 'UTF-8')
+    . ';height:' . htmlspecialchars($height, ENT_QUOTES, 'UTF-8') . ';';
+?>
+<div id="<?php echo $containerId; ?>" style="<?php echo $style; ?>"></div>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum/build/pannellum.css">
 <script src="https://cdn.jsdelivr.net/npm/pannellum/build/pannellum.js"></script>
 
 <script>
-// Initialize viewer with JSON config from helper
-document.addEventListener("DOMContentLoaded", function() {
-    pannellum.viewer("<?php echo $containerId; ?>", <?php echo $config; ?>);
-});
+    document.addEventListener("DOMContentLoaded", function () {
+        pannellum.viewer("<?php echo $containerId; ?>", <?php echo $config; ?>);
+    });
 </script>
